61eda6af593c099f2e56ef9571ba55ddb4572a53
amadesya <154593967+amadesya@users.noreply.github.com>
2025-01-24 10:54:31 +0300

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите имя файла");
+var fileName = Console.ReadLine();
+
+FileInfo fileInfo = new FileInfo(fileName);
+if (fileInfo.Exists)
+{
+    Console.WriteLine($"Файл с именем {fileName} существует");
+    using (StreamReader reader = new StreamReader(fileInfo.FullName))
+    {
+        string text = await reader.ReadToEndAsync();
+        Console.WriteLine(text);
+    }
+}
+else
+{
+    Console.WriteLine($"Файл с именем {fileName} не существует");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("Введите имя файла");
+var fileName = Console.ReadLine();
+
+FileInfo fileInfo = new FileInfo(fileName);
+if (fileInfo.Exists)
+{
+    Console.WriteLine($"Файл с именем {fileName} существует");
+
+}
+else
+{
+    Console.WriteLine($"Файл с именем {fileName} не существует, файл будет создан");
+    fileInfo.Create();
+}
