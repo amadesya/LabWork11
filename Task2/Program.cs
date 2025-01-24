7d66b779f3b7f07264e5943b7b7f6fc267779073
amadesya <154593967+amadesya@users.noreply.github.com>
2025-01-24 11:25:19 +0300

    Task 2
    Task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,13 +1,20 @@
-﻿using System.Security.Cryptography;
-
-Console.WriteLine("Введите имя файла");
+﻿Console.WriteLine("Введите имя файла");
 var fileName = Console.ReadLine();
 
 FileInfo fileInfo = new FileInfo(fileName);
 if (fileInfo.Exists)
 {
     Console.WriteLine($"Файл с именем {fileName} существует");
-
+    using (StreamWriter writer = new StreamWriter(fileName, true))
+    {
+        while(true)
+        {
+            string lines = Console.ReadLine();
+            if (lines == "end")
+                break;
+            writer.WriteLine(lines);
+        }
+    }
 }
 else
 {
--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61eda6af593c099f2e56ef9571ba55ddb4572a53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
